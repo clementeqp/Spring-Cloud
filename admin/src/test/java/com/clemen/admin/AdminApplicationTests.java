ab280d8e91baed676f6a71c0765a7618c4de7b6f
Clemente <cquintap@emeal.nttdata.com>
2022-04-07 19:25:21 +0200

    Crear Eureka Admin

---
+++ admin/src/test/java/com/clemen/admin/AdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clemen.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ spring-config/Readme.md
@@ -0,0 +1,3 @@
+Configuramos con un *.yml para que lo carge antes.
+En nuestro github creamos una carpeta config-data
+http://localhost:8081/bootstrap/default
